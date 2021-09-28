3913a9a4b9a55bafe4976a6069fe31e9977c2fda
Oath <82308226+apisit-kmitl@users.noreply.github.com>
2021-09-28 21:42:06 +0700

    index

--- pages/reviewfood/index.jsx
+++ pages/reviewfood/index.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 function index() {
     return (
-        <div>
-            review food
-        </div>
+        <>
+            <div className="flex h-screen w-screen bg-gray-900 items-center justify-center" > 
+              <h1  className="px-20 py-7 bg-gray-50 text-3xl text-gray-600 rounded-2xl"  >`กูคือปุ่มม </h1>
+             </div>
+        </>
     )
 }
 
