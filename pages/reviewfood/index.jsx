6a10921b065b296aafca0bafbd239019ca774f23
Oath <82308226+apisit-kmitl@users.noreply.github.com>
2021-09-28 21:52:24 +0700

    5555

--- pages/reviewfood/index.jsx
+++ pages/reviewfood/index.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
+import React from "react";
 
 function index() {
-    return (
-        <>
-            <div className="flex h-screen w-screen bg-gray-900 items-center justify-center" > 
-              <h1  className="px-20 py-7 bg-gray-50 text-3xl text-gray-600 rounded-2xl"  >กูคือปุ่มม </h1>
-             </div>
-        </>
-    )
+  return (
+    <>
+      <div className="flex h-screen w-screen bg-gray-900 items-center justify-center">
+        <h1 className="px-20 py-7 bg-gray-50 text-3xl text-gray-600 rounded-2xl">
+          กูคือปุ่มม{" "}
+        </h1>
+      </div>
+    </>
+  );
 }
-
-export default index
+export default index;
