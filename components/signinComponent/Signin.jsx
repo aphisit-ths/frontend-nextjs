35ad7a83c2cad793f83f1af3265c9f6340e5e187
Oath <82308226+apisit-kmitl@users.noreply.github.com>
2021-09-28 18:36:39 +0700

    edit push page

--- components/signinComponent/Signin.jsx
+++ components/signinComponent/Signin.jsx
@@ -48,7 +48,7 @@ function Signin() {
             email: "",
             password: "",
           });
-        Router.push("/products");
+        Router.push("/select-topic");
       }
     },
   });
