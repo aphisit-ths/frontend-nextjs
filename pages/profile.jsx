8b19f4694221b32cf1aca1ef385898276091c5d3
teeranaisilk <88429693+teeranaisilk@users.noreply.github.com>
2021-11-15 12:50:07 +0700

    profile page

---
+++ pages/profile.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import HomePage from "../components/homepage/HomePage"
+export default function index() {
+    return (
+        <div>
+            <HomePage></HomePage>
+        </div>
+    )
+}
