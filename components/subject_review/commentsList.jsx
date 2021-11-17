d3e60b4a9dc1961b2b9ca5f92190ee80f6b6359f
Oath <82308226+apisit-kmitl@users.noreply.github.com>
2021-11-17 08:33:42 +0700

    Merge branch 'dev'

--- components/subject_review/commentsList.jsx
+++ components/subject_review/commentsList.jsx
@@ -39,12 +39,12 @@ export default function Preview_Comments() {
   const { subjectComments } = data;
   
   return (
-    <div>
-      <div className="flex flex-col items-center p-1 overflow-ellipsis overflow-hidden justify-center md:p-10">
-        <div className=" bg-gray-50 w-5/6 md:w-2/3   mt-5 py-4 px-3 md:px-4 ">
-          <div className="flex justify-between">
-            <div className="flex flex-col">
-              <p className="font-display  text-lg">แนะนำล่าสุด</p>
+    
+      <div className="flex flex-col w-full md:w-4/5  items-center p-1 justify-center md:p-10">
+        <div className=" bg-gray-50 flex w-4/5  md:w-3/5 justify-between  mt-5 py-4 px-3 md:px-4 ">
+          <div className="flex w-full  justify-between ">
+            <div className="flex flex-col ">
+              <p className="font-display text-lg">แนะนำล่าสุด</p>
               <div className=" w-28 rounded-2xl h-1 bg-green-300"></div>
             </div>
             <Link href="/createsubject" passHref >
@@ -56,13 +56,14 @@ export default function Preview_Comments() {
             </Link>
           </div>
         </div>
+
         {subjectComments.map((comment, index) => (
           <>
             <Link href={"reviewsubjects/" + comment.subjectId.id} passHref>
               <motion.div
                 whileTap={{ scale: 0.8 }}
                 key={comment.subjectId.id}
-                className="bg-gray-50 w-5/6 md:w-2/3  max-w-sm md:max-w-3xl overflow-ellipsis overflow-hidden   max-h-full rounded-xl flex flex-col my-3  p-6 px-2 transition   cursor-pointer r shadow-lg space-y-3 hover:bg-gray-100  duration-200 "
+                className="bg-gray-50 w-full md:w-3/5  max-h-full rounded-xl flex flex-col my-3  p-6 px-2 transition   cursor-pointer r shadow-lg space-y-3 hover:bg-gray-100  duration-200 "
               >
                 <div className="  inline-flex py-2 w-2/3 flex-warp font-display items-center  z-30 px-2 xl:px-6  ">
                   {comment.subjectId.course_id}{" "}
@@ -99,6 +100,6 @@ export default function Preview_Comments() {
           </>
         ))}
       </div>
-    </div>
+    
   );
 }
